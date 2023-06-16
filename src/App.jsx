aa377c8325a838b41d72ee320b4aa47b6d731e61
jamaal <palacebikes@gmail.com>
2023-06-15 20:00:04 -0700

    added color picker feature and some new styling as well

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-// Write your Color component here
+import { useState } from 'react';
+import './index.css';
+
+const Color = ({ color, setSelectedColor, selectedColor }) => {
+  return (
+    <div
+      className={color === selectedColor ? `${color} selected` : color}
+      onClick={() => setSelectedColor(color)}
+    ></div>
+  );
+};
+
+
 
 const App = () => {
+
+  const [selectedColor, setSelectedColor] = useState("")
+
   return (
+
+    <div id="app">
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="violet" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/>
+        <Color color ="red" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/>
+        <Color color="black" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/>
+      </div>
     </div>
+  </div>
+
   );
 };
 
